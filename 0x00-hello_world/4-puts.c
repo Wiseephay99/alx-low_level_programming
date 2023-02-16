15737367ca5cdbc41aac269deadad8d33f452d6c
Wiseephay99 <wiseephay99@gmail.com>
2023-02-16 13:05:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle followed byy a new line."
- * return 0.
+ * puzzle followed by a new line.
+ * Return: Always 0.
  */
 
 int main(void)
-
 {
     puts("\"Programming is like building a multilingual puzzle");
-
     return (0);
 }
