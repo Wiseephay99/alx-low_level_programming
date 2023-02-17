dc52d143eadf291e36dce85f50014f61e9081670
Wiseephay99 <wiseephay99@gmail.com>
2023-02-17 02:49:23 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
- 
- /** 
- * main - prints a text according to number
- * Return: Always (Success)
- *
- */
-
+/** 
+* main - prints a text according to number
+* Return: Always (Success)
+*
+*/
 int main(void)
 {
 int n, lastd;
