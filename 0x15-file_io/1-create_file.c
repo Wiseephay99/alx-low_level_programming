0ec309a2aa9902bde3df75c3e7cc9a9547198847
Wiseephay99 <wiseephay99@gmail.com>
2023-10-02 02:33:40 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @filename: A pointer name of the file to create.
+ * @text_content: A pointer to a strings to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
